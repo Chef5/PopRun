5dae8bc7f3f6a649938ece2d9920a09126815659
Patrick-Jun <nunet@qq.com>
2020-03-19 22:02:15 +0800

    暂时完成个人中心顶部布局

--- pages/user/user.js
+++ pages/user/user.js
@@ -6,8 +6,44 @@ Page({
      * 页面的初始数据
      */
     data: {
+        user: {},  //用户数据
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        let that = this;
+        // 初始化用户数据
+        let user = that.getUserInfoFromLocal();
+        if(user){
+            that.updateCurrentUser(user);//本地有数据
+        }else{
+            //从服务器获取：判断是否注册、本地缓存
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
     },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
     /**
      * 注册并获取用户信息
      */
@@ -45,59 +81,19 @@ Page({
         )
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    // 从本地获取用户数据
+    getUserInfoFromLocal: function () {
+        let user = wx.getStorageSync('user');
+        if(user){
+            return JSON.parse(user);
+        }else return false;
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    // 更新用户显示数据
+    updateCurrentUser: function(data){
+        if(data){
+            this.setData({user: data});
+        }
     }
+
 })
