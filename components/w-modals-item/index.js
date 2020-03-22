138479de6b0add824f75e5d49a1b87b37c5162c8
Patrick-Jun <nunet@qq.com>
2020-03-22 22:26:05 +0800

    勋章组件开发

--- components/w-modals-item/index.js
+++ components/w-modals-item/index.js
@@ -4,14 +4,28 @@ Component({
      * 组件的属性列表
      */
     properties: {
-
+        medal: {
+            type: Object,  //数据类型
+            value: null    //默认值
+        }
     },
 
     /**
      * 组件的初始数据
      */
     data: {
+        data: {}  //用于展示
+    },
 
+    /**
+     * 生命周期
+     */
+    attached: function() {
+        let that = this;
+        //整理传入数据
+        that.setData({
+            data: that.properties.medal
+        })
     },
 
     /**
--- pages/user/user.js
+++ pages/user/user.js
@@ -7,6 +7,80 @@ Page({
      */
     data: {
         user: {},  //用户数据
+        medals: [
+            {
+                "linkid": 1,
+                "rid": 7,
+                "meid": 1,
+                "created_at": "2020-03-21 17:22:52",
+                "updated_at": "2020-03-21 17:22:55",
+                "mkey": "star_1_act",
+                "type": 0,
+                "name": "1星跑者",
+                "desc": "单次运动里程达5km",
+                "img": "http://127.0.0.1:8000/resources/medals/star_1_act.png"
+              },
+              {
+                "linkid": 3,
+                "rid": 7,
+                "meid": 10,
+                "created_at": "2020-03-21 17:23:54",
+                "updated_at": "2020-03-21 17:23:58",
+                "mkey": "2020_02",
+                "type": 0,
+                "name": "2020.02",
+                "desc": "您2020.02累计跑步7次，授予您2020.02月活跃勋章",
+                "img": "http://127.0.0.1:8000/resources/medals/2020_02.png"
+              },
+              {
+                "linkid": 4,
+                "rid": 7,
+                "meid": 11,
+                "created_at": "2020-03-21 17:24:04",
+                "updated_at": "2020-03-21 17:24:06",
+                "mkey": "2020_03",
+                "type": 0,
+                "name": "2020.03",
+                "desc": "您2020.03累计跑步7次，授予您2020.03月活跃勋章",
+                "img": "http://127.0.0.1:8000/resources/medals/2020_03.png"
+              },
+              {
+                "linkid": 5,
+                "rid": 7,
+                "meid": 21,
+                "created_at": "2020-03-21 17:24:41",
+                "updated_at": "2020-03-21 17:24:45",
+                "mkey": "2020_a",
+                "type": 0,
+                "name": "Spring2020",
+                "desc": "您2020年春季累计跑步45次，授予您Spring2020活跃勋章",
+                "img": "http://127.0.0.1:8000/resources/medals/2020_a.png"
+              },
+              {
+                "linkid": 6,
+                "rid": 7,
+                "meid": 25,
+                "created_at": "2020-03-21 17:24:53",
+                "updated_at": "2020-03-21 17:24:56",
+                "mkey": "rank_a",
+                "type": 1,
+                "name": "青铜",
+                "desc": "您在上月累计里程在校区前100名，授予您一枚青铜勋章",
+                "img": "http://127.0.0.1:8000/resources/medals/rank_a.png"
+              },
+              {
+                "linkid": 7,
+                "rid": 7,
+                "meid": 25,
+                "created_at": "2020-03-21 17:27:51",
+                "updated_at": "2020-03-21 17:27:53",
+                "mkey": "rank_a",
+                "type": 1,
+                "name": "青铜",
+                "desc": "您在上月累计里程在校区前100名，授予您一枚青铜勋章",
+                "img": "http://127.0.0.1:8000/resources/medals/rank_a.png"
+              }
+        ]
     },
 
     /**
